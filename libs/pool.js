d5b46034c5bc22ecf5551f08bf68aea0a3b79c1d
Lewis Freiberg <lewis.freiberg@cheproximity.com.au>
2017-06-01 14:41:07 +1000

    Leave default examples in the pool

--- libs/pool.js
+++ libs/pool.js
@@ -1,3 +1,8 @@
+// Phantom Pool
+// https://github.com/binded/phantom-pool
+// Uses an implimentation of Generic Pool to
+// handle high rate calls on resources.
+
 const createPhantomPool = require('phantom-pool').default
 
 const pool = () => {
@@ -8,8 +13,12 @@ const pool = () => {
     idleTimeoutMillis: 30000, // default.
     // maximum number of times an individual resource can be reused before being destroyed; set to 0 to disable
     maxUses: 50, // default
+    // function to validate an instance prior to use; see https://github.com/coopernurse/node-pool#createpool
+    validator: () => Promise.resolve(true), // defaults to always resolving true
+    // validate resource before borrowing; required for `maxUses and `validator`
+    testOnBorrow: true, // default
     // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
-    phantomArgs: [['--ignore-ssl-errors=true', '--disk-cache=true']] // arguments passed to phantomjs-node directly, default is `[]`. For all opts, see https://github.com/amir20/phantomjs-node#phantom-object-api
+    phantomArgs: [['--ignore-ssl-errors=true', '--disk-cache=true']]
   })
 }
 
