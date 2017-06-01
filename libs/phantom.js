1d14a860420d449d0a12b688d695f5c01caafe82
Lewis Freiberg <lewis.freiberg@cheproximity.com.au>
2017-06-01 14:41:15 +1000

    Add description

--- libs/phantom.js
+++ libs/phantom.js
@@ -1,73 +1,51 @@
-// const phantom = require('phantom')
+// PhantomJS
+// https://github.com/amir20/phantomjs-node
+// Uses PhantomJS to render a page to base64 then return it.
+// No require for Phantom because of 'pool'
+
 const pool = require('./pool')
-const log = console.log
 
 module.exports = async (url, size, crop) => {
-  return await pool.use(async instance => {
-    const page = await instance.createPage()
-    // Set the viewport
-    await page.property('viewportSize', {
-      width: size.width,
-      height: size.height
-    })
-    // Set a Standard User Agent
-    await page.setting(
-      'userAgent',
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    )
-    // Request Resource
-    await page.on('onResourceRequested', function (requestData) {
-      console.log('Requesting', requestData.url)
-    })
-
-    if (crop) {
-      await page.property('clipRect', {
-        top: 0,
-        left: 0,
+  const image = await pool.use(async instance => {
+    let image
+    let status
+    try {
+      const page = await instance.createPage()
+      // Set the viewport
+      await page.property('viewportSize', {
         width: size.width,
         height: size.height
       })
-    }
+      // Set a Standard User Agent
+      await page.setting(
+        'userAgent',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      )
+      // Request Resource
+      await page.on('onResourceRequested', function (requestData) {
+        console.log('Requesting', requestData.url)
+      })
 
-    const status = await page.open(url)
-    const image = await page.renderBase64()
-    await instance.exit()
+      if (crop) {
+        await page.property('clipRect', {
+          top: 0,
+          left: 0,
+          width: size.width,
+          height: size.height
+        })
+      }
+
+      status = await page.open(url)
+      image = await page.renderBase64()
+    } catch (e) {
+      console.log(e)
+      image = ''
+      status = 'Errror'
+    }
 
     console.log('Returned page with:', status)
     return image
   })
+
+  return image
 }
-// module.exports = async (url, size, crop) => {
-//   const instance = await phantom.create(['--ignore-ssl-errors=yes'], {
-//     logger: { warn: log, debug: log, error: log }
-//   })
-//   const page = await instance.createPage()
-//   // Set the viewport
-//   await page.property('viewportSize', {
-//     width: size.width,
-//     height: size.height
-//   })
-//   // Set a Standard User Agent
-//   await page.setting(
-//     'userAgent',
-//     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-//   )
-//   // Request Resource
-//   await page.on('onResourceRequested', function (requestData) {
-//     console.log('Requesting', requestData.url)
-//   })
-
-//   await page.property('clipRect', {
-//     top: 0,
-//     left: 0,
-//     width: size.width,
-//     height: size.height
-//   })
-
-//   const status = await page.open(url)
-//   const image = await page.renderBase64()
-//   await instance.exit()
-
-//   console.log('Returned page with:', status)
-//   return image
-// }
